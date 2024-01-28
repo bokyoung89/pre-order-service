773a03d6ad9c62791a6f524d33ec48d6e7932e27
bokyoung shin <sbk8689@gmail.com>
2024-01-28 10:31:16 +0900

    #6 - 회원가입, 로그인 테스트 코드 작성
    회원가입, 로그인 테스트 코드 작성(UserContrillerTest, UserServiceTest)
    세부적인 예외 상황 추가 작성 필요

---
+++ src/test/java/com/bokyoung/preorderservice/controller/UserControllerTest.java
@@ -0,0 +1,214 @@
+package com.bokyoung.preorderservice.controller;
+
+import com.bokyoung.preorderservice.controller.request.UserLoginRequest;
+import com.bokyoung.preorderservice.exception.ErrorCode;
+import com.bokyoung.preorderservice.model.User;
+import com.bokyoung.preorderservice.controller.request.UserJoinRequest;
+import com.bokyoung.preorderservice.exception.PreOrderServiceException;
+import com.bokyoung.preorderservice.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.DisabledIf;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Disabled
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void 회원가입_성공() throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        when(userService.join(email, password, nickname, greeting, profile_image, emailVerified)).thenReturn(mock(User.class));
+
+        //then
+        mockMvc.perform(post("/api/auth/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                // TODO : add reqeust body
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(email, password, nickname, greeting, profile_image, emailVerified)))
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void 회원가입시_이미_회원가입된_email로_회원가입을_시도하면_에러반환 () throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        // TODO : develop
+        when(userService.join(email, password, nickname, greeting, profile_image, emailVerified)).thenThrow(new PreOrderServiceException(ErrorCode.DUPLICATED_EMAIL));
+
+        //then
+        mockMvc.perform(post("/api/auth/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(email, password, nickname, greeting, profile_image, emailVerified)))
+        ).andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Disabled
+    @Test
+    public void 회원가입시_입력값이_유효하지_않으면_에러반환 () throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        // TODO : develop
+        when(userService.join(email, password, nickname, greeting, profile_image, emailVerified)).thenThrow(new PreOrderServiceException(ErrorCode.DUPLICATED_EMAIL));
+
+        //then
+        mockMvc.perform(post("/api/auth/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(email, password, nickname, greeting, profile_image, emailVerified)))
+                ).andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Disabled
+    @Test
+    public void 회원가입시_메일인증번호가_유효하지_않으면_에러반환 () throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        // TODO : develop
+        when(userService.join(email, password, nickname, greeting, profile_image, emailVerified)).thenThrow(new PreOrderServiceException(ErrorCode.DUPLICATED_EMAIL));
+
+        //then
+        mockMvc.perform(post("/api/auth/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(email, password, nickname, greeting, profile_image, emailVerified)))
+                ).andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Disabled
+    @Test
+    public void 회원가입시_메일인증_완료 () throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        // TODO : develop
+        when(userService.join(email, password, nickname, greeting, profile_image, emailVerified)).thenThrow(new PreOrderServiceException(ErrorCode.DUPLICATED_EMAIL));
+
+        //then
+        mockMvc.perform(post("/api/auth/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(email, password, nickname, greeting, profile_image, emailVerified)))
+                ).andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void 로그인_성공() throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+
+        //when
+        when(userService.login(email, password)).thenReturn("test_token");
+
+        //then
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(email, password)))
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void 로그인_시_회원가입이_안된_email을_입력할경우_에러반환() throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+
+        //when
+        when(userService.login(email, password)).thenThrow(new PreOrderServiceException(ErrorCode.USER_NOT_FOUND));
+
+        //then
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(email, password)))
+                ).andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void 로그인_시_틀린_password를_입력할경우_에러반환() throws Exception {
+        //given
+        String email = "username";
+        String password = "password";
+
+        //when
+        when(userService.login(email, password)).thenThrow(new PreOrderServiceException(ErrorCode.INVALID_PASSWORD));
+
+        //then
+        mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // TODO : add reqeust body
+                        .content(objectMapper.writeValueAsBytes(new UserLoginRequest(email, password)))
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+}
+
---
+++ src/test/java/com/bokyoung/preorderservice/service/UserServiceTest.java
@@ -0,0 +1,181 @@
+package com.bokyoung.preorderservice.service;
+
+import com.bokyoung.preorderservice.domain.UserAccount;
+import com.bokyoung.preorderservice.exception.PreOrderServiceException;
+import com.bokyoung.preorderservice.fixture.UserAccountFixture;
+import com.bokyoung.preorderservice.repository.UserAccountRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Disabled
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserAccountRepository userAccountRepository;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Test
+    void 회원가입이_정상적으로_동작하는_경우() {
+        //given
+        String email = "username";
+        String password = "password";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        //mocking
+        when(userAccountRepository.findByEmail(email)).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(password)).thenReturn("encrypt_password");
+        when(userAccountRepository.save(any())).thenReturn(UserAccountFixture.get(email, password));
+
+        //then
+        Assertions.assertDoesNotThrow(() -> userService.join(email, password, nickname, greeting, profile_image, emailVerified));
+    }
+
+    @Test
+    void 회원가입시_email로_회원가입한_유저가_이미_있는경우() {
+        //given
+        String email = "sbk8689@gmail.com";
+        String password = "qwer1234!";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+        UserAccount fixture = UserAccountFixture.get(email, password);
+
+        //when
+        //mocking
+        when(userAccountRepository.findByEmail(email)).thenReturn(Optional.of(fixture));
+        when(passwordEncoder.encode(password)).thenReturn("encrypt_password");
+        when(userAccountRepository.save(any())).thenReturn(Optional.of(fixture));
+
+        //then
+        Assertions.assertThrows(PreOrderServiceException.class, () -> userService.join(email, password, nickname, greeting, profile_image, emailVerified));
+    }
+
+    @Test
+    void 회원가입된_유저가_메일인증을_하지_않았을_경우_ () throws Exception {
+        //given
+        //when
+        //then
+    }
+
+    @Disabled
+    @Test
+    void 회원가입시_입력값이_유효하지_않은경우() {
+        //given
+        String email = "sbk8689@gmail.com";
+        String password = "qwer1234!";
+        String nickname = "신보경";
+        String greeting = "안녕하세요!";
+        String profile_image = "";
+        Boolean emailVerified = false;
+
+        //when
+        // TODO : mocking
+
+        //then
+        Assertions.assertThrows(PreOrderServiceException.class, () -> userService.join(email, password, nickname, greeting, profile_image, emailVerified));
+    }
+
+
+    @Disabled
+    @Test
+    void 회원가입시_메일인증번호가_유효하지_않은_경우() {
+        //given
+        // TODO : implement
+
+        //when
+        // TODO : mocking
+
+        //then
+        // TODO : implement
+        //Assertions.assertDoesNotThrow(() -> ;
+    }
+
+    @Disabled
+    @Test
+    void 회원가입시_메일인증번호가_정상적인_경우() {
+        //given
+        // TODO : implement
+
+        //when
+        // TODO : mocking
+
+        //then
+        // TODO : implement
+        //Assertions.assertDoesNotThrow(() -> ;
+    }
+
+    @Test
+    void 로그인이_정상적으로_동작하는_경우() {
+        //given
+        String email = "username";
+        String password = "password";
+        UserAccount fixture = UserAccountFixture.get(email, password);
+
+        //when
+        when(userAccountRepository.findByEmail(email)).thenReturn(Optional.of(fixture));
+        when(passwordEncoder.matches(password, fixture.getPassword())).thenReturn(true);
+
+        //then
+        Assertions.assertDoesNotThrow(() -> userService.login(email, password));
+    }
+
+    @Test
+    void 로그인시_email로_회원가입한_유저가_없는_경우() {
+        //given
+        String email = "sbk8689@gmail.com";
+        String password = "qwer1234!";
+        UserAccount fixture = UserAccountFixture.get(email, password);
+
+        //when
+        //mocking
+        when(userAccountRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        //then
+        Assertions.assertThrows(PreOrderServiceException.class, () -> userService.login(email, password));
+    }
+
+    @Test
+    void 로그인시_password가_틀린_경우() {
+        //given
+        String email = "sbk8689@gmail.com";
+        String password = "qwer1234!";
+        String wrongPassword = "wrongPassword";
+        UserAccount fixture = UserAccountFixture.get(email, password);
+
+        //when
+        //mocking
+        when(userAccountRepository.findByEmail(email)).thenReturn(Optional.of(fixture));
+
+        //then
+        Assertions.assertThrows(PreOrderServiceException.class, () -> userService.login(email, wrongPassword));
+    }
+
+}
